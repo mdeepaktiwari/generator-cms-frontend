fd361b854f091b4782e1193c63b671a4dc219a55
Deepak Tiwari <110996070+mdeepaktiwari@users.noreply.github.com>
2025-12-20 19:32:59 +0530

    feat: set up project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<div>login</div>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
